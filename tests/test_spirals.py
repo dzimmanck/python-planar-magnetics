90f84e59108f1a80bf74b48413e80d075e3bf93a
Donny Zimmanck <dzimmanck@enphaseenergy.com>
2022-04-07 15:26:22 -0700

    Code cleanup.

--- tests/test_spirals.py
+++ tests/test_spirals.py
@@ -3,19 +3,26 @@ from planar_magnetics.windings import Spiral
 from planar_magnetics.geometry import Point
 import math
 
-
+# test for issue-10
 def test_single_turn_resistance_estimation():
+    inner_radius = 100
+    outer_radius = 101
+    thickness = 0.1
+
     # create large thin single turn
     spiral = Spiral(
-        Point(0, 0), inner_radius=100, outer_radius=101, num_turns=1, gap=0.1
+        at=Point(0, 0),
+        inner_radius=inner_radius,
+        outer_radius=outer_radius,
+        num_turns=1,
+        gap=0.1,
     )
 
     # estimate the resistance using the estimate_dcr method
-    dcr = spiral.estimate_dcr(thickness=0.1)
+    dcr = spiral.estimate_dcr(thickness=thickness)
 
-    # estimate the resistance using the resistane of annulus helper function
-    expected = dcr_of_annulus(0.1, 100, 101)
-    print(dcr, expected)
+    # estimate the resistance using the resistance of annulus helper function
+    expected = dcr_of_annulus(thickness, inner_radius, outer_radius)
     assert math.isclose(dcr, expected)
 
 
