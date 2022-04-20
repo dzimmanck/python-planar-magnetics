adc5a6b2babb78929cb7c9018eca2bcf16a825cb
Donny Zimmanck <dzimmanck@enphaseenergy.com>
2022-04-20 14:10:08 -0700

    Added some utilities for calculating skin depths.

--- planar_magnetics/utils.py
+++ planar_magnetics/utils.py
@@ -1,10 +1,11 @@
 import math
+from planar_magnetics.materials import Conductor, COPPER
 
 
 def weight_to_thickness(weight: float):
     """Converter a copper weight to a thickness
 
-    Converters a copper weight (oz) to a thickess (meters)
+    Converters a copper weight (oz) to a thickness (meters)
 
     Args:
         weight: weight of copper layer in oz
@@ -14,6 +15,21 @@ def weight_to_thickness(weight: float):
     """
 
     return 35e-6 * weight
+
+
+def thickness_to_weight(thickness: float):
+    """Converter a copper thickness to a weight
+
+    Converters a copper thickness (m) to a weight (oz)
+
+    Args:
+        thickness: thickness of the layer in m
+
+    Returns:
+        float: The weight of the layer in oz
+    """
+
+    return 28571.428571428572 * thickness
 
 
 def dcr_of_sheet(thickness: float, width: float, length: float, rho: float = 1.68e-8):
@@ -38,3 +54,34 @@ def dcr_of_annulus(
     )
 
     return resistance
+
+
+def frequency_to_skin_depth(
+    frequency: float, temperature: float = 25, material: Conductor = COPPER
+):
+    """Calculate the skin depth at a specific frequency
+    """
+
+    rho = material.get_resistivity(temperature)
+    mu = material.permeability
+    skin_depth = math.sqrt(2 * rho / (2 * math.pi * frequency * mu))
+
+    return skin_depth
+
+
+def skin_depth_to_frequency(
+    skin_depth: float, temperature: float = 25, material: Conductor = COPPER
+):
+    """Calculate the frequency at a specific skin depth
+    """
+
+    rho = material.get_resistivity(temperature)
+    mu = material.permeability
+    frequency = 2 * rho / (2 * math.pi * skin_depth ** 2 * mu)
+
+    return frequency
+
+
+if __name__ == "__main__":
+    frequency = skin_depth_to_frequency(weight_to_thickness(4))
+    print(frequency)
